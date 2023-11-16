0be5e03bddf82d6ad144253fe52af4fde19c02ee
morelir <58606266+morelir@users.noreply.github.com>
2023-11-16 18:09:08 +0200

    update

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function loadGame() {
   main.appendChild(button);
   var rules = document.createElement("p");
   rules.textContent =
-    "Numbers will fall... if you have a keyboard (preferably with a numpad), press the correct keys to knock them away before they hit the ground";
+    "Numbers will fall... drag the bucket and catch numbers from 1 to 10 before they hit the ground";
   main.appendChild(rules);
   button.addEventListener("click", function startIt(e) {
     document.querySelector(".start-game").classList.add("indeed");
@@ -16,51 +16,20 @@ function loadGame() {
   });
 }
 
-function playAgain(e) {
-  document.querySelector(".game-over").classList.remove("indeed");
-  playGame();
-}
-
-// function createCustomCursor() {
-//   const main = document.querySelector("#main");
-//   const cursor = document.createElement("div");
-//   cursor.className = "custom-cursor";
-//   main.appendChild(cursor);
-
-//   main.addEventListener("mousemove", function (e) {
-//     const x = e.clientX;
-//     const y = e.clientY;
-//     cursor.style.transform = `translate(${x - 20}px, ${y - 120}px)`;
-//   });
-// }
-
 function playGame(replay) {
   document.documentElement.classList.add("playing");
-  const LETTERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const LETTERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
   let orderCount = 1;
   const animations = {};
   let gameOn = true;
-  var timeOffset = 500; // 2000 interval between letters starting, will be faster over time
-  var DURATION = 3000; //10000
+  var timeOffset = 3000; // interval between letters starting, will be faster over time
+  var DURATION = 3000; 
   var main = document.querySelector("#main");
-  var header = document.querySelector("header");
-  var scoreElement = document.getElementById("score");
   let draggable;
-  var score = parseFloat(scoreElement.textContent);
   var rate = 1;
   var RATE_INTERVAL = 0; //playbackRate will increase by .05 for each letter... so after 20 letters, the rate of falling will be 2x what it was at the start
-  var misses = 0;
   const intervalIds = [];
-  const eventListeners = {};
-  createBucket(animations);
-
-  // LETTERS.forEach(function (l) {
-  //   animations[l] = [];
-  // });
-
-  var audioCtx;
-  var oscillator;
-  var gainNode;
+  createBucket();
 
   function createBucket() {
     let mousePosition;
@@ -68,7 +37,6 @@ function playGame(replay) {
     let isDown = false;
 
     draggable = document.createElement("div");
-    // draggable.classList.add("draggable");
     draggable.style.position = "absolute";
     draggable.style.left = "50%";
     draggable.style.top = "100%";
@@ -79,35 +47,26 @@ function playGame(replay) {
     draggable.style.backgroundSize = "100px 100px";
     draggable.style.zIndex = "1";
     draggable.style.cursor = "grab";
-
     main.appendChild(draggable);
 
-    // addEventListenerWithTracking(draggable, "mousedown", function (e) {
-    //   isDown = true;
-    //   offset = [
-    //     draggable.offsetLeft - e.clientX,
-    //     draggable.offsetTop - e.clientY,
-    //   ];
-    //   e.preventDefault();
-    // });
-    draggable.addEventListener("mousedown", function (e) {
+    draggable.addEventListener("mousedown", function (event) {
       isDown = true;
       offset = [
-        draggable.offsetLeft - e.clientX,
-        draggable.offsetTop - e.clientY,
+        draggable.offsetLeft - event.clientX,
+        draggable.offsetTop - event.clientY,
       ];
-      e.preventDefault();
+      draggable.style.cursor = "grabbing";
+      event.preventDefault();
     });
 
     document.addEventListener("mouseup", function () {
-      console.log("mouseup");
       isDown = false;
+      draggable.style.cursor = "grab";
     });
 
-    main.addEventListener("mousemove", function (event) {
+    document.addEventListener("mousemove", function (event) {
       event.preventDefault();
       if (isDown) {
-        console.log(event.clientX, event.clientY);
         mousePosition = {
           x: event.clientX,
           y: event.clientY,
@@ -122,7 +81,7 @@ function playGame(replay) {
   function create() {
     var lettersIdx = Math.floor(Math.random() * LETTERS.length);
     const elementId = Date.now();
-    var x = Math.random() * 85 + "vw";
+    var x = Math.random() * 100 + "vw";
     var container = document.createElement("div");
     container.id = elementId;
     container.style.zIndex = 1;
@@ -144,10 +103,6 @@ function playGame(replay) {
       }
     );
 
-    // animations[LETTERS[lettersIdx]].splice(0, 0, {
-    //   animation: animation,
-    //   element: container,
-    // });
     animations[elementId] = {
       animation: animation,
       element: container,
@@ -159,25 +114,35 @@ function playGame(replay) {
     //If an animation finishes, we will consider that as a miss, so we will remove it from the active animations array and increment our miss count
     animation.onfinish = function (e) {
       const target = container;
-      // var char = target.textContent;
       delete animations[elementId];
-      // animations[char].pop();
       target.classList.add("missed");
-      // handleMisses();
     };
   }
 
-  //When a miss is registered, check if we have reached the max number of misses
-  // function handleMisses() {
-  //   misses++;
-  //   var missedMarker = document.querySelector(".misses:not(.missed)");
-  //   console.log("miss");
-  //   if (missedMarker) {
-  //     missedMarker.classList.add("missed");
-  //   } else {
-  //     gameOver();
-  //   }
-  // }
+  function win() {
+    gameOn = false;
+    for (let i = 0; i < intervalIds.length; i++) {
+      clearInterval(intervalIds[i]);
+    }
+    getAllAnimations().forEach(function (anim) {
+      anim.pause();
+    });
+
+    const endGame = document.querySelector(".end-game");
+    endGame.classList.add("indeed");
+    main.textContent = "";
+    const p = document.createElement("p");
+    p.textContent = "You Win!";
+    const button = document.createElement("button");
+    button.textContent = "Start Again";
+    endGame.append(p, button);
+
+    button.addEventListener("click", function () {
+      document.querySelector(".end-game").classList.remove("indeed");
+      endGame.removeChild(button);
+      playGame();
+    });
+  }
 
   //End game and show screen
   function gameOver() {
@@ -185,27 +150,23 @@ function playGame(replay) {
     for (let i = 0; i < intervalIds.length; i++) {
       clearInterval(intervalIds[i]);
     }
-    // clearInterval(cleanupInterval);
     getAllAnimations().forEach(function (anim) {
       anim.pause();
     });
 
-    //Could use Web Animations API here, but showing how you can use a mix of Web Animations API and CSS transistions
-    // document.body.removeEventListener('keypress', onPress);
-    // oscillator.stop(0);
-
-    const gameOver = document.querySelector(".game-over");
-    gameOver.classList.add("indeed");
+    const endGame = document.querySelector(".end-game");
+    endGame.classList.add("indeed");
     main.textContent = "";
+    const p = document.createElement("p");
+    p.textContent = "Game Over";
     const button = document.createElement("button");
-    button.textContent="Start Again"
-    gameOver.appendChild(button)
-
+    button.textContent = "Start Again ";
+    endGame.append(p, button);
 
     button.addEventListener("click", function () {
-      document.querySelector(".game-over").classList.remove("indeed");
+      document.querySelector(".end-game").classList.remove("indeed");
+      endGame.textContent = "";
       playGame();
-      gameOver.removeChild(button);
     });
   }
 
@@ -225,7 +186,8 @@ function playGame(replay) {
   //Periodically do for all dynamic falling numbers, if Draggable(bucket) div is over a target div(number)
   intervalIds.push(
     setInterval(function () {
-      Object.keys(animations).forEach(function (key) {
+      const keys = Object.keys(animations);
+      for (const key of keys) {
         const { element: target, animation } = animations[key];
         if (isOverlapping(draggable, target)) {
           // execute when the draggable div hovers over a target div
@@ -233,6 +195,8 @@ function playGame(replay) {
           const num = +targetText.textContent;
           if (orderCount !== num) {
             gameOver();
+          } else if (num === 10) {
+            win();
           }
           orderCount++;
           console.log("Draggable div is over a target div");
@@ -255,21 +219,20 @@ function playGame(replay) {
           );
           target.classList.add("missed");
         }
-      });
-    }, 100)
+      }
+    }, 0)
   );
 
+  // check if draggable div hovers over a target div
   function isOverlapping(draggable, target) {
     const rect1 = draggable.getBoundingClientRect();
     const rect2 = target.getBoundingClientRect();
-
-    const fixedNum = 20;
+    
     return (
-      rect1.top < rect2.bottom + fixedNum + 50 &&
-      rect1.top > rect2.bottom - fixedNum &&
+      rect1.top < rect2.bottom  &&
       rect1.bottom > rect2.top &&
-      rect1.left < rect2.right &&
-      rect1.right > rect2.left
+      rect1.left < rect2.right  &&
+      rect1.right > rect2.left 
     );
   }
 
@@ -283,147 +246,20 @@ function playGame(replay) {
     return [];
   }
 
-  //On key press, see if it matches an active animating (falling) letter. If so, pop it from active array, pause it (to keep it from triggering "finish" logic), and add an animation on inner element with random 3d rotations that look like the letter is being kicked away to the distance. Also update score.
-  // function onPress(e) {
-  //   var char = e.key;
-  //   if (char.length === 1) {
-  //     char = char.toLowerCase();
-  //     if (animations[char] && animations[char].length) {
-  //       var popped = animations[char].pop();
-  //       popped.animation.pause();
-  //       var target = popped.element.querySelector("b");
-  //       var degs = [
-  //         Math.random() * 1000 - 500,
-  //         Math.random() * 1000 - 500,
-  //         Math.random() * 2000 - 1000,
-  //       ];
-  //       target.animate(
-  //         [
-  //           {
-  //             transform: "scale(1) rotateX(0deg) rotateY(0deg) rotateZ(0deg)",
-  //             opacity: 1,
-  //           },
-  //           {
-  //             transform:
-  //               "scale(0) rotateX(" +
-  //               degs[0] +
-  //               "deg) rotateY(" +
-  //               degs[1] +
-  //               "deg) rotateZ(" +
-  //               degs[2] +
-  //               "deg)",
-  //             opacity: 0,
-  //           },
-  //         ],
-  //         {
-  //           duration: Math.random() * 500 + 850,
-  //           easing: "ease-out",
-  //           fill: "both",
-  //         }
-  //       );
-  //       addScore();
-  //       playSound(LETTERS.indexOf(char) / LETTERS.length);
-  //       header.textContent += char;
-  //     }
-  //   }
-  // }
-  // function addScore() {
-  //   score++;
-  //   scoreElement.textContent = score;
-  // }
-
-  // document.body.addEventListener("keypress", onPress);
-
   //start the letters falling... create the element+animation, and setup timeout for next letter to start
   function setupNextLetter() {
     if (gameOn) {
-      create();
+      for (let i = 0; i < 15; i++) {
+        setTimeout(() => {
+          if (gameOn) {
+            create();
+          }
+        }, 100 * i);
+      }
       setTimeout(function () {
         setupNextLetter();
       }, timeOffset);
     }
   }
   setupNextLetter();
-
-  function addEventListenerWithTracking(element, type, listener) {
-    element.addEventListener(type, listener);
-    if (!eventListeners[type]) {
-      eventListeners[type] = [];
-    }
-    eventListeners[type].push(listener);
-  }
-
-  function removeAllEventListeners(type) {
-    if (eventListeners[type]) {
-      eventListeners[type].forEach((listener) => {
-        element.removeEventListener(type, listener);
-      });
-    }
-  }
-
-  // var isPlaying;
-  // var maxVol = .025;
-  // var minVol = 0.0000001;
-  // function setupAudio() {
-
-  //   if (window.AudioContext || window.webkitAudioContext) {
-  //     audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  //     oscillator = audioCtx.createOscillator();
-  //     gainNode = audioCtx.createGain();
-
-  //     oscillator.connect(gainNode);
-
-  //     // set options for the oscillator
-  //     oscillator.type = 'sawtooth';
-  //     oscillator.frequency.value = 880; // value in hertz
-  //     oscillator.detune.value = 100; // value in cents
-  //     oscillator.start(0);
-
-  //     gainNode.gain.value = minVol;
-
-  //     document.querySelector('footer button').addEventListener('click', function(e) {
-  //       if (isPlaying) {
-  //         gainNode.disconnect(audioCtx.destination);
-  //         e.currentTarget.textContent = 'Sound Off';
-  //       } else {
-  //         gainNode.connect(audioCtx.destination);
-  //         e.currentTarget.textContent = 'Sound On';
-  //       }
-
-  //       isPlaying = !isPlaying;
-  //     });
-  //   }
-  // }
-  // function playSound(offset) {
-  //   if (isPlaying) {
-  //     gainNode.gain.setValueAtTime(maxVol, audioCtx.currentTime);
-  //     oscillator.frequency.setValueAtTime(440 + (offset * 440), audioCtx.currentTime);
-  //     gainNode.gain.linearRampToValueAtTime(minVol, audioCtx.currentTime + .4);
-  //     oscillator.frequency.linearRampToValueAtTime(220 - (offset * 110), audioCtx.currentTime + .4);
-  //   }
-  // }
-
-  // setupAudio();
 }
-
-// if (isDown) {
-//   mousePosition = {
-//     x: event.clientX,
-//     y: event.clientY,
-//   };
-//   let newX = mousePosition.x - offset[0];
-//   let newY = mousePosition.y - offset[1];
-//   // console.log("mousePosition.x:"+mousePosition.x , mousePosition.y)
-//   // console.log("offset[0]:"+offset[0],offset[1])
-//   // console.log("newX:"+newX,newY)
-
-//   console.log("div:"+div.offsetWidth,"main:"+main.offsetWidth)
-//   console.log(offset[0],offset[1])
-//   if (newX >= 0 && newX <= main.offsetWidth - div.offsetWidth) {
-//     div.style.left = newX + "px";
-//   }
-
-//   if (newY >= 0 && newY <= main.offsetHeight - div.offsetHeight) {
-//     div.style.top = newY + "px";
-//   }
-// }
