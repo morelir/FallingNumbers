02a8b9ea8e69ae15b4ca4238d92f49db463e0eca
morelir <58606266+morelir@users.noreply.github.com>
2023-11-22 23:11:24 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// "use strict";
 loadGame();
 
 function loadGame() {
@@ -19,7 +20,7 @@ function loadGame() {
 function playGame(replay) {
   document.documentElement.classList.add("playing");
   const LETTERS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-  let orderCount = 1;
+  let nextNumber = 1;
   const animations = {};
   let gameOn = true;
   var timeOffset = 3000; // interval between letters starting, will be faster over time
@@ -30,6 +31,7 @@ function playGame(replay) {
   var RATE_INTERVAL = 0; //playbackRate will increase by .05 for each letter... so after 20 letters, the rate of falling will be 2x what it was at the start
   const intervalIds = [];
   createBucket();
+  createNextElement();
 
   function createBucket() {
     let offset = [0, 0];
@@ -50,7 +52,6 @@ function playGame(replay) {
     main.appendChild(draggable);
 
     draggable.addEventListener("mousedown", function (event) {
-      console.log(draggable.offsetLeft - event.clientX);
       isDown = true;
       offset = [
         draggable.offsetLeft - event.clientX,
@@ -64,6 +65,11 @@ function playGame(replay) {
       isDown = false;
       draggable.style.cursor = "grab";
     });
+
+    document.addEventListener("touchmove", function (event) {
+      event.preventDefault();
+      console.log(true)
+    })
 
     document.addEventListener("mousemove", function (event) {
       event.preventDefault();
@@ -97,7 +103,6 @@ function playGame(replay) {
 
     // check if draggable inside horizontal boundaries of the container
     function draggableInHorizontalBoundaries(mouseX, draggableRect) {
-      console.log(draggable.offsetLeft);
       return (
         mouseX - (draggable.offsetLeft - offset[0] - draggableRect.left) >=
           mainRect.left &&
@@ -117,8 +122,21 @@ function playGame(replay) {
     }
   }
 
+  function createNextElement(){
+    const header = document.querySelector("header");
+    const next = document.createElement("p");
+    const span = document.createElement("span");
+    next.innerText="Next: ";
+    next.classList.add("next-number");
+    span.innerText=nextNumber;
+    next.appendChild(span)
+    header.appendChild(next)
+    
+  }
+
+
   //Create a letter element and setup its falling animation, add the animation to the active animation array, and setup an onfinish handler that will represent a miss.
-  function create() {
+  function createLetter() {
     var lettersIdx = Math.floor(Math.random() * LETTERS.length);
     const elementId = Date.now();
     var x = Math.random() * 100 + "vw";
@@ -157,6 +175,80 @@ function playGame(replay) {
       delete animations[elementId];
       target.classList.add("missed");
     };
+  }
+
+ 
+
+  //Periodically remove missed elements, and lower the interval between falling elements
+  intervalIds.push(
+    setInterval(function () {
+      timeOffset = (timeOffset * 4) / 5;
+      cleanup();
+    }, 20000)
+  );
+  function cleanup() {
+    [].slice.call(main.querySelectorAll(".missed")).forEach(function (missed) {
+      main.removeChild(missed);
+    });
+  }
+
+  //Periodically does for all dynamic falling numbers, if Draggable(bucket) div is over on one of them, than does the appropriate action
+  intervalIds.push(
+    setInterval(function () {
+      const keys = Object.keys(animations);
+      for (const key of keys) {
+        const { element: target, animation } = animations[key];
+        if (isOverlapping(draggable, target)) {
+          // execute when the draggable div hovers over a target div
+          const targetText = target.querySelector("b");
+          const num = +targetText.textContent;
+          if (nextNumber !== num) {
+            gameOver();
+          } else if (num === 10) {
+            win();
+          }
+          nextNumber++;
+          delete animations[key];
+          animation.pause();
+
+          targetText.animate(
+            [
+              {
+                opacity: 1,
+              },
+              {
+                opacity: 0,
+              },
+            ],
+            {
+              easing: "ease-out",
+              fill: "both",
+            }
+          );
+          target.classList.add("missed");
+          stepNextNumber();
+
+        }
+      }
+    }, 0)
+  );
+
+  function stepNextNumber(){
+    const spanNextNumber = document.querySelector("header .next-number span")
+    spanNextNumber.innerText = nextNumber;
+  }
+
+  // check if draggable div hovers over a target div
+  function isOverlapping(draggable, target) {
+    const rect1 = draggable.getBoundingClientRect();
+    const rect2 = target.getBoundingClientRect();
+
+    return (
+      rect1.top < rect2.bottom &&
+      rect1.bottom > rect2.top &&
+      rect1.left < rect2.right &&
+      rect1.right > rect2.left
+    );
   }
 
   function win() {
@@ -210,71 +302,6 @@ function playGame(replay) {
     });
   }
 
-  //Periodically remove missed elements, and lower the interval between falling elements
-  intervalIds.push(
-    setInterval(function () {
-      timeOffset = (timeOffset * 4) / 5;
-      cleanup();
-    }, 20000)
-  );
-  function cleanup() {
-    [].slice.call(main.querySelectorAll(".missed")).forEach(function (missed) {
-      main.removeChild(missed);
-    });
-  }
-
-  //Periodically does for all dynamic falling numbers, if Draggable(bucket) div is over on one of them, than does the appropriate action
-  intervalIds.push(
-    setInterval(function () {
-      const keys = Object.keys(animations);
-      for (const key of keys) {
-        const { element: target, animation } = animations[key];
-        if (isOverlapping(draggable, target)) {
-          // execute when the draggable div hovers over a target div
-          const targetText = target.querySelector("b");
-          const num = +targetText.textContent;
-          if (orderCount !== num) {
-            gameOver();
-          } else if (num === 10) {
-            win();
-          }
-          orderCount++;
-          delete animations[key];
-          animation.pause();
-
-          targetText.animate(
-            [
-              {
-                opacity: 1,
-              },
-              {
-                opacity: 0,
-              },
-            ],
-            {
-              easing: "ease-out",
-              fill: "both",
-            }
-          );
-          target.classList.add("missed");
-        }
-      }
-    }, 0)
-  );
-
-  // check if draggable div hovers over a target div
-  function isOverlapping(draggable, target) {
-    const rect1 = draggable.getBoundingClientRect();
-    const rect2 = target.getBoundingClientRect();
-
-    return (
-      rect1.top < rect2.bottom &&
-      rect1.bottom > rect2.top &&
-      rect1.left < rect2.right &&
-      rect1.right > rect2.left
-    );
-  }
-
   //Firefox 48 supports document.getAnimations as per latest spec, Chrome 52 and polyfill use older spec
   function getAllAnimations() {
     if (document.getAnimations) {
@@ -291,7 +318,7 @@ function playGame(replay) {
       for (let i = 0; i < 15; i++) {
         setTimeout(() => {
           if (gameOn) {
-            create();
+            createLetter();
           }
         }, 100 * i);
       }
